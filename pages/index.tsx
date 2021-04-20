6076dfee1c21e664a568997709cb164ba9e8137b
nakanonatski <nn.616718@gmail.com>
2021-04-20 23:21:57 +0900

    skill page

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,13 +10,13 @@ export default function Home() {
       </Head>
 
       <header className="p-4 w-full flex flex-row bg-blue-50 fixed z-10">
-        <a className="flex-auto text-4xl font-bold ml-20">portfolio</a>
+        <a className="flex-auto text-4xl font-bold ml-20"><a href="#">portfolio</a></a>
         <nav className="flex-auto mr-20">
           <ul className="flex justify-end">
-            <li className="pr-8 text-2xl">about</li>
-            <li className="pr-8 text-2xl">skill</li>
-            <li className="pr-8 text-2xl">work</li>
-            <li className="text-2xl">contact</li>
+            <li className="pr-8 text-2xl"><a href="#about">about</a></li>
+            <li className="pr-8 text-2xl"><a href="#skill">skill</a></li>
+            <li className="pr-8 text-2xl"><a href="#work">work</a></li>
+            <li className="text-2xl"><a href="#contact">contact</a></li>
           </ul>
         </nav>
       </header>
@@ -28,7 +28,7 @@ export default function Home() {
       <div className="container mx-auto">
         <main className="">
           <section className="h-screen">
-            <div className="text-center text-5xl pt-32 pb-20">
+            <div id="about" className="text-center text-5xl pt-32 pb-20">
               <h2>about</h2>
             </div>
             <div className="flex flex-row">
@@ -43,20 +43,56 @@ export default function Home() {
             </div>
           </section>
           <section className="h-screen bg-blue-50 w-screen m-header-menu">
-            <div className="text-center text-5xl pt-32 pb-20">
+            <div id="skill" className="text-center text-5xl pt-32 pb-20">
               <h2>skill</h2>
             </div>
             <div className="grid grid-cols-2 mx-20 gap-x-20 gap-y-10">
-              <div className="rounded-lg shadow-md bg-white h-36">1</div>
-              <div className="rounded-lg shadow-md bg-white h-36">2</div>
-              <div className="rounded-lg shadow-md bg-white h-36">3</div>
-              <div className="rounded-lg shadow-md bg-white h-36">4</div>
-              <div className="rounded-lg shadow-md bg-white h-36">5</div>
+              <div className="rounded-lg shadow-md bg-white h-36">
+                <h3 className="text-center py-4">フロントエンド</h3>
+                <ul className="px-20">
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>JavaScript</li>
+                </ul>
+              </div>
+              <div className="rounded-lg shadow-md bg-white h-36">
+                <h3 className="text-center py-4">バックエンド</h3>
+                <ul className="px-20">
+                  <li>PHP</li>
+                  <li>Java</li>
+                </ul>
+              </div>
+              <div className="rounded-lg shadow-md bg-white h-36">
+              <h3 className="text-center py-4">フレームワーク</h3>
+                <ul className="px-20">
+                  <li>CakePHP</li>
+                  <li>Next.js</li>
+                </ul>
+              </div>
+              <div className="rounded-lg shadow-md bg-white h-36">
+              <h3 className="text-center py-4">DB</h3>
+                <ul className="px-20">
+                  <li>PostgreSQL</li>
+                </ul>
+              </div>
+              <div className="rounded-lg shadow-md bg-white h-36">
+              <h3 className="text-center py-4">インフラ</h3>
+                <ul className="px-20">
+                  <li>RedHat</li>
+                  <li>Apache</li>
+                  <li>Docker</li>
+                </ul>
+              </div>
             </div>
           </section>
           <section className="h-screen">
-          <div className="text-center text-5xl pt-32 pb-20">
+            <div id="work" className="text-center text-5xl pt-32 pb-20">
               <h2>work</h2>
+            </div>
+          </section>
+          <section className="h-screen">
+            <div id="contact" className="text-center text-5xl pt-32 pb-20">
+              <h2>contact</h2>
             </div>
           </section>
         </main>
