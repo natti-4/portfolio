c4a421f0c0006419a05896c496f698cdecdc889b
nakanonatski <nn.616718@gmail.com>
2021-08-15 16:30:22 +0900

    google analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import Header from '../components/Header'
 import Main from '../components/Main'
 import Footer from '../components/Footer'
@@ -6,11 +5,6 @@ import Footer from '../components/Footer'
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>portfolio</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <Header/>
 
       <div className="container mx-auto">
